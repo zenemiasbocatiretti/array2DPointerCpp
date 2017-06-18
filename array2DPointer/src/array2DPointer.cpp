6b47ae96d7573eb98b474e6bb1848e559423d083
Marcelo Bittencourt <tchelopoa@gmail.com>
2017-06-18 18:17:58 -0300

    Update value of array using a function passing array by argument

--- array2DPointer/src/array2DPointer.cpp
+++ array2DPointer/src/array2DPointer.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+bool updateValue(int rowNumber, int colNumber, int newValue, int** a) {
+	cout << "Trying to update value of the array using a function..." << endl;
+	a[rowNumber][colNumber] = newValue;
+	return true;
+}
 
+void showArrayValues(int numberOfRowsCols, int** a) {
+	cout << "Showing values from the array using a function..." << endl;
+	for(int i = 0; i<numberOfRowsCols; ++i) {
+		for(int j = 0; j<numberOfRowsCols; ++j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main () {
 
 	//set row and colCount (2 x 2 - two rows and two columns)
 	int rowColCount = 2;
@@ -20,16 +35,15 @@ int main () {
 	array2D[1][0] = 30;
 	array2D[1][1] = 40;
 
-	cout << "Showing values in the array..." << endl;
-	for(int i = 0; i<rowColCount; ++i) {
-		for(int j = 0; j<rowColCount; ++j) {
-			cout << array2D[i][j] << " ";
-		}
-		cout << endl;
-	}
+	showArrayValues(rowColCount, array2D);
+
+	bool result;
+	result = updateValue(0, 1, 25, array2D);
+	if ( result ) cout << "Value successfully updated!" << endl;
+
+	showArrayValues(rowColCount, array2D);
 
 	cout << "Cleaning up the array from memory..." << endl;
-
 	for(int i = 0; i < rowColCount; ++i) {
 	    delete [] array2D[i];
 	}
